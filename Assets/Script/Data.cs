f78fb88f88f032ab11f8a0bbabb21e92973df2e9
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2014-11-13 10:06:23 +0700

    add all required component

--- Assets/Script/Data.cs
+++ Assets/Script/Data.cs
@@ -4,12 +4,12 @@ using System.Collections.Generic;
 
 public class StoryData
 {
-		public static string defaultThaiAnimation = "idle";
-		public static Dictionary<string, List<AnimationData>> storyThaiData = new Dictionary<string, List<AnimationData>> ();
+		public static string defaultAnimation = "idle";
+		public static Dictionary<string, Dictionary<string, StorySet>> storyData = new Dictionary<string, Dictionary<string, StorySet>> ();
 
 		public static void InitData ()
 		{
-				List<AnimationData> introThaiData = new List<AnimationData> ();
+				List<AnimationData> animationDataList = new List<AnimationData> ();
 				AnimationData animationData;
 
 				animationData = new AnimationData ();
@@ -17,15 +17,40 @@ public class StoryData
 				animationData.animationLength = 3.0f;
 				animationData.positionX = -5.3f;
 				animationData.imageName = "island";
-				introThaiData.Add (animationData);
+				animationDataList.Add (animationData);
 		
 				animationData = new AnimationData ();
 				animationData.animationName = "respect";
-				//				animationData.scaleX = -1f;
+				animationData.animationLength = 3.0f;
 				animationData.imageName = "flag";
+				animationData.sound = "th_sawasdee";
+				animationData.text = "Hello";
+				animationDataList.Add (animationData);
+		
+				animationData = new AnimationData ();
+				animationData.animationName = "respect";
+				animationData.animationLength = 3.0f;
+				animationData.imageName = "night";
+				animationData.sound = "th_thankyou";
+				animationData.text = "Thank you";
+				animationData.autoProceed = true;
+				animationDataList.Add (animationData);
+		
+				animationData = new AnimationData ();
+				animationData.animationName = "walk";
+				animationData.imageName = "night";
+				animationData.animationDelay = 0.5f;
+				animationData.animationLength = 3.0f;
+				animationData.positionX = -9f;
+				animationData.scaleX = -1f;
+				animationDataList.Add (animationData);
 
-				introThaiData.Add (animationData);
-				storyThaiData ["intro"] = introThaiData;
+				StorySet storySet = new StorySet ();
+				storySet.animationDataList = animationDataList;
+				storySet.bgm = "ค้างคาวกินกล้วย";
+
+				storyData ["th"] = new Dictionary<string, StorySet> ();
+				storyData ["th"] ["intro"] = storySet;
 		}
 }
 
@@ -33,8 +58,17 @@ public class AnimationData
 {
 		public string animationName;
 		public float animationLength;
+		public float animationDelay;
 		public float? positionX;
 		public float? scaleX;
 		public string imageName;
+		public string sound;
 		public string text;
+		public bool autoProceed;
+}
+
+public class StorySet
+{
+		public List<AnimationData> animationDataList;
+		public string bgm;
 }
