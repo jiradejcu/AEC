b2a9c9c9cdc25713b19b9fc3de1c4be13a243043
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2014-11-27 12:55:38 +0700

    add new UI

--- Assets/Script/Back.cs
+++ Assets/Script/Back.cs
@@ -1,17 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.UI; 
 using System.Collections;
 
 public class Back : MonoBehaviour
 {
+		Button button;
+		Text text;
+
+		void Start ()
+		{
+				button = GetComponent<Button> ();
+				text = GetComponentInChildren<Text> ();
+		}
+
 		void Update ()
 		{
 				if (Application.loadedLevelName.Equals ("SelectStory") && string.IsNullOrEmpty (Main.selectedCountry)) {
-						renderer.enabled = false;
-				} else
-						renderer.enabled = true;
+						button.enabled = false;
+						button.image.enabled = false;
+						text.enabled = false;
+				} else {
+						button.enabled = true;
+						button.image.enabled = true;
+						text.enabled = true;
+				}
 		}
 
-		void OnMouseDown ()
+		public void OnClick ()
 		{
 				if (Application.loadedLevelName.Equals ("Main"))
 						Application.LoadLevel ("SelectStory");
