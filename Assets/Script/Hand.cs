f46119152d08b70badfcfe088e67b6efcb3eaa0d
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2014-11-28 18:01:39 +0700

    adjust subtitle position

--- Assets/Script/Hand.cs
+++ Assets/Script/Hand.cs
@@ -1,22 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Hand : MonoBehaviour
 {
-		static Vector3 smallScale = new Vector3 (0.6f, 0.6f);
-		static Vector3 moveBy = new Vector3 (-0.1f, 0.1f);
-		static float fadeFrom = 0.5f;
+		static Vector3 smallScale = new Vector3 (0.8f, 0.8f);
+		static Vector3 moveBy = new Vector3 (-0.15f, 0.15f);
 		static float time = 0.5f;
 		bool previousCountdown;
 		bool previousRenderer;
+		Image image;
 
 		void Start ()
 		{
+				image = GetComponent<Image> ();
 				previousCountdown = Main.countdown.HasValue;
-				previousRenderer = renderer.enabled;
+				previousRenderer = image.enabled;
 				iTween.ScaleTo (gameObject, iTween.Hash ("scale", smallScale, "time", time, "easetype", iTween.EaseType.linear, "looptype", iTween.LoopType.pingPong));
 				iTween.MoveBy (gameObject, iTween.Hash ("amount", moveBy, "time", time, "easetype", iTween.EaseType.linear, "looptype", iTween.LoopType.pingPong));
-				iTween.FadeFrom (gameObject, iTween.Hash ("amount", fadeFrom, "time", time, "easetype", iTween.EaseType.easeInSine, "looptype", iTween.LoopType.pingPong));
 		}
 
 		void Update ()
@@ -24,10 +25,10 @@ public class Hand : MonoBehaviour
 				if (previousCountdown != Main.countdown.HasValue || previousRenderer != Main.HasNext) {
 						previousCountdown = Main.countdown.HasValue;
 
-						renderer.enabled = Main.HasNext;
-						previousRenderer = renderer.enabled;
-
-						if (renderer.enabled)
+						image.enabled = Main.HasNext;
+						previousRenderer = image.enabled;
+			
+						if (image.enabled)
 								iTween.Resume (gameObject);
 						else
 								iTween.Pause (gameObject);
