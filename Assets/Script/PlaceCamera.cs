7eb3bf0a75ff863242870df749e25a4a42126154
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2015-01-12 23:23:26 +0700

    add camera fade

--- Assets/Script/PlaceCamera.cs
+++ Assets/Script/PlaceCamera.cs
@@ -4,12 +4,20 @@ using System.Collections;
 public class PlaceCamera : MonoBehaviour
 {
 		GameObject targetGameObject;
-		static float flyTime = 2f;
+		static float flyTime = 3f;
+
+		void Start ()
+		{
+				Hashtable ht = iTween.Hash ("amount", new Vector3 (0f, 0.3f), "easetype", iTween.EaseType.easeInOutSine, "looptype", iTween.LoopType.pingPong);
+				iTween.MoveBy (gameObject, ht); 
+		}
 
 		public void SetTargetPosition (GameObject targetGameObject)
 		{
 				this.targetGameObject = targetGameObject;
 				iTween.LookTo (gameObject, targetGameObject.transform.position, flyTime);
 				iTween.MoveTo (gameObject, targetGameObject.transform.position, flyTime);
+				iTween.CameraFadeAdd (iTween.CameraTexture (Color.white));
+				iTween.CameraFadeTo (iTween.Hash("amount", 1f, "delay", 0.5f, "time", flyTime - 1f));
 		}
 }
