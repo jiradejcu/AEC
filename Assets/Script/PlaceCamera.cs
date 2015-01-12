9fe88d08e8e9696f8eb64643f6e8e561f77a47f8
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2015-01-12 22:33:38 +0700

    add fly to place feature

---
+++ Assets/Script/PlaceCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceCamera : MonoBehaviour
+{
+		GameObject targetGameObject;
+		static float flyTime = 2f;
+
+		public void SetTargetPosition (GameObject targetGameObject)
+		{
+				this.targetGameObject = targetGameObject;
+				iTween.LookTo (gameObject, targetGameObject.transform.position, flyTime);
+				iTween.MoveTo (gameObject, targetGameObject.transform.position, flyTime);
+		}
+}
