53e100ce0fee25d13af2a1f1b6a566a665d654a4
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2015-01-22 11:10:31 +0700

    config select place scene

--- Assets/Script/CommonConfig.cs
+++ Assets/Script/CommonConfig.cs
@@ -25,8 +25,8 @@ public class CommonConfig
 		public static string ANSWER_CORRECT = "answer_correct";
 		public static string ANSWER_WRONG = "answer_wrong";
 		public static string SCORE_SUMMARY = "score_summary";
-		public static bool TEST_MODE = false;
-		public static bool OFFLINE_MODE = true;
+		public static bool TEST_MODE = true;
+		public static bool OFFLINE_MODE = false;
 		public static Dictionary<string, Vector2> CENTRAL_COORDINATE = new Dictionary<string, Vector2> ();
 		public static Dictionary<string, float> MAP_SCALE = new Dictionary<string, float> ();
 		public static Dictionary<string, string> URL_MAPPING = new Dictionary<string, string> ();
@@ -47,7 +47,34 @@ public class CommonConfig
 
 		public static void Init ()
 		{
-				CENTRAL_COORDINATE.Add (COUNTRY_CODE.th.ToString (), new Vector2 (13.141246f, 101.520955f));
-				MAP_SCALE.Add (COUNTRY_CODE.th.ToString (), 4.5f);
+				CENTRAL_COORDINATE.Add (COUNTRY_CODE.bn.ToString (), new Vector2 (4.561211f, 114.712760f));
+				MAP_SCALE.Add (COUNTRY_CODE.bn.ToString (), 60f);
+
+				CENTRAL_COORDINATE.Add (COUNTRY_CODE.kh.ToString (), new Vector2 (12.600536f, 104.986637f));
+				MAP_SCALE.Add (COUNTRY_CODE.kh.ToString (), 14.5f);
+
+				CENTRAL_COORDINATE.Add (COUNTRY_CODE.id.ToString (), new Vector2 (-2.461119f, 117.839082f));
+				MAP_SCALE.Add (COUNTRY_CODE.id.ToString (), 1.85f);
+
+				CENTRAL_COORDINATE.Add (COUNTRY_CODE.la.ToString (), new Vector2 (18.227167f, 103.867049f));
+				MAP_SCALE.Add (COUNTRY_CODE.la.ToString (), 8.55f);
+
+				CENTRAL_COORDINATE.Add (COUNTRY_CODE.my.ToString (), new Vector2 (3.886775f, 109.521332f));
+				MAP_SCALE.Add (COUNTRY_CODE.my.ToString (), 3.83f);
+
+				CENTRAL_COORDINATE.Add (COUNTRY_CODE.mm.ToString (), new Vector2 (19.260256f, 96.780903f));
+				MAP_SCALE.Add (COUNTRY_CODE.mm.ToString (), 4.27f);
+
+				CENTRAL_COORDINATE.Add (COUNTRY_CODE.ph.ToString (), new Vector2 (13.098078f, 121.764821f));
+				MAP_SCALE.Add (COUNTRY_CODE.ph.ToString (), 5f);
+
+				CENTRAL_COORDINATE.Add (COUNTRY_CODE.sg.ToString (), new Vector2 (1.323338f, 103.848287f));
+				MAP_SCALE.Add (COUNTRY_CODE.sg.ToString (), 130f);
+
+				CENTRAL_COORDINATE.Add (COUNTRY_CODE.th.ToString (), new Vector2 (13.141246f, 101.620955f));
+				MAP_SCALE.Add (COUNTRY_CODE.th.ToString (), 4.8f);
+		
+				CENTRAL_COORDINATE.Add (COUNTRY_CODE.vn.ToString (), new Vector2 (16.079912f, 105.852973f));
+				MAP_SCALE.Add (COUNTRY_CODE.vn.ToString (), 5f);
 		}
 }
--- Assets/Script/PlaceCamera.cs
+++ Assets/Script/PlaceCamera.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlaceCamera : MonoBehaviour
 {
 		public static float flyTime = 3f;
+		public GameObject map;
 
 		void Start ()
 		{
 				Hashtable ht = iTween.Hash ("amount", new Vector3 (0f, 1f), "easetype", iTween.EaseType.easeInOutSine, "looptype", iTween.LoopType.pingPong);
-				iTween.MoveBy (gameObject, ht); 
+				iTween.MoveBy (gameObject, ht);
+				Image image = map.GetComponent<Image> ();
+				image.sprite = Resources.Load<Sprite> ("Image/Map/" + Main.selectedCountry);
 		}
 
 		public void SetTargetPosition (GameObject targetGameObject)
@@ -16,6 +20,6 @@ public class PlaceCamera : MonoBehaviour
 				iTween.LookTo (gameObject, targetGameObject.transform.position, flyTime);
 				iTween.MoveTo (gameObject, targetGameObject.transform.position, flyTime);
 				iTween.CameraFadeAdd (iTween.CameraTexture (Color.white));
-				iTween.CameraFadeTo (iTween.Hash("amount", 1f, "delay", 0.5f, "time", flyTime - 1f));
+				iTween.CameraFadeTo (iTween.Hash ("amount", 1f, "delay", 0.5f, "time", flyTime - 1f));
 		}
 }
